3190d07a92ade8746dd73cf8beec645c9b1578ad
Divyanshu <dnd.mein@gmail.com>
2026-06-21 17:30:00 +0530

    name configured

--- lib/pinecone.ts
+++ lib/pinecone.ts
@@ -4,4 +4,4 @@ export const pinecone=new Pinecone({
     apiKey:process.env.PINECONE_DB_API_KEY!
 })
 
-export const pineconeIndex=pinecone.index("coderabbit")
+export const pineconeIndex=pinecone.index("codehorse")
